2a529c092f8eea03c3cf48704eee7cd6bb17925b
Patrick Sande <patricktsande@gmail.com>
2018-10-18 08:51:57 -0500

    updates to HW10

--- HW10.sql
+++ HW10.sql
@@ -1,14 +1,33 @@
 use sakila;
+-- Q1a.  Display the first and last names of all actors from the table actor
 SELECT first_name, last_name FROM actor;
-
+-- Q1b.  Display First & Last name of all actors in a single column in Uppercase
 SELECT CONCAT(first_name,' ', last_name) AS Actor_Name FROM actor;
-
+-- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." 
+-- What is one query would you use to obtain this information?
 SELECT actor_id, first_name, last_name FROM actor
 WHERE first_name = "joe";
-
+-- 2b. Find all actors whose last name contain the letters GEN:
 SELECT first_name, last_name FROM actor
 WHERE last_name LIKE "%GEN%";
-
+-- 2c. Find all actors whose last names contain the letters LI. Order the rows by last name and first name:
 SELECT first_name, last_name FROM actor
 WHERE last_name LIKE "%LI%"
 ORDER BY last_name, first_name;
+-- 2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
+SELECT country_id, country FROM country
+WHERE country IN ('Afghanistan','Bangladesh','China');
+-- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, 
+-- so create a column in the table actor named description and use the data type BLOB 
+-- (Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
+
+-- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
+
+-- 4a. List the last names of actors, as well as how many actors have that last name.
+
+-- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+
+-- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
+
+-- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! 
+-- In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
